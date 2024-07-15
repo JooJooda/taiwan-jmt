b7b593bebe6350a8a10bdc9f8f0ae031d88863d9
JooJooda <liyingzhou375@gmail.com>
2024-07-15 17:55:05 +0900

    feat : revise API for Food model

--- foods/serializers.py
+++ foods/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Food
+from .models import *
+
+class ThumbnailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Thumbnail
+        fields = "__all__"
 
 class FoodSerializer(serializers.ModelSerializer):
+    # 역참조 필드 시리얼라이저
+    thumbnails = ThumbnailSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Food
         fields = "__all__"
