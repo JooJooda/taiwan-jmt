16db5539aed2bd84d37a668c1449c7eb9138644c
JooJooda <liyingzhou375@gmail.com>
2024-07-16 23:28:27 +0900

    feat : implement API for User model

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from .models import *
+from .serializers import *
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import *
 
-# Create your views here.
+class UserDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        user = get_object_or_404(User, id=user.id)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+        
